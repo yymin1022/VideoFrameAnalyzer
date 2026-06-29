81c043859d38c6c64b0bbed6a310cd13374fbf1a
Useful <useful@vimosoft.com>
2026-06-29 18:03:46 +0900

    Update Main Screen : Added video select ui

--- app/src/main/java/com/yong/videoanalyzer/main/MainScreen.kt
+++ app/src/main/java/com/yong/videoanalyzer/main/MainScreen.kt
@@ -1,11 +1,21 @@
 package com.yong.videoanalyzer.main
 
-import androidx.compose.foundation.layout.Box
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+private const val MIMETYPE_VIDEO = "video/*"
 
 /**
  * Main Screen UI
@@ -18,9 +28,49 @@ fun MainScreen(
     // UI State
     val uiState by viewModel.uiState.collectAsState()
 
-    Box(
-        modifier = modifier,
+    val context = LocalContext.current
+
+    // Launche system file picker
+    val videoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.OpenDocument(),
+    ) { uri -> uri?.let { viewModel.onVideoSelected(it, context) } }
+
+    Column(
+        modifier = modifier
+            .padding(8.dp),
     ) {
-        Text("Main Screen")
+        VideoSelect(
+            modifier = Modifier,
+            videoFileName = uiState.videoFileName,
+            onBtnSelectVideo = { videoPickerLauncher.launch(arrayOf(MIMETYPE_VIDEO)) },
+        )
+    }
+}
+
+@Composable
+private fun VideoSelect(
+    modifier: Modifier = Modifier,
+    videoFileName: String?,
+    onBtnSelectVideo: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(),
+            onClick = onBtnSelectVideo,
+        ) {
+            Text("Select Video")
+        }
+
+        videoFileName?.let {
+            Text(
+                modifier = Modifier,
+                text = "Selected [$it]",
+            )
+        }
     }
 }
